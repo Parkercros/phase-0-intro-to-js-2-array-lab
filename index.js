70ec0f80b3af37f1dde495dae9b6eecf44babb53
Parker Crosgrove <pdxparker3@gmail.com>
2023-01-31 21:10:23 -0700

    finished test

--- index.js
+++ index.js
@@ -1,53 +1,33 @@
-// Define the initial cats array
-let cats = ["Milo", "Otis", "Garfield"];
+const cats = ["Milo", "Otis", "Garfield"];
 
-// Define the destructivelyAppendCat function
-function destructivelyAppendCat(name) {
-    cats.push(name);
+function destructivelyAppendCat (name) {
+  cats.push(name);
 }
 
-// Define the destructivelyPrependCat function
-function destructivelyPrependCat(name) {
-    cats.unshift(name);
+function destructivelyPrependCat (name) {
+  cats.unshift(name);
 }
 
-// Define the destructivelyRemoveLastCat function
-function destructivelyRemoveLastCat() {
-    cats.pop();
+function destructivelyRemoveLastCat () {
+  cats.pop();
 }
 
-// Define the destructivelyRemoveFirstCat function
-function destructivelyRemoveFirstCat() {
-    cats.shift();
+function destructivelyRemoveFirstCat () {
+  cats.shift();
 }
 
-// Define the appendCat function
-function appendCat(name) {
-    let newCats = cats.slice();
-    newCats.push(name);
-    return newCats;
+function appendCat (name) {
+  return [...cats, name];
 }
 
-// Define the prependCat function
-function prependCat(name) {
-    let newCats = cats.slice();
-    newCats.unshift(name);
-    return newCats;
+function prependCat (name) {
+  return [name, ...cats];
 }
 
-// Define the removeLastCat function
-function removeLastCat() {
-    let newCats = cats.slice();
-    newCats.pop();
-    return newCats;
+function removeFirstCat () {
+  return cats.slice(1);
 }
 
-// Define the removeFirstCat function
-function removeFirstCat() {
-    let newCats = cats.slice();
-    newCats.shift();
-    return newCats;
+function removeLastCat () {
+  return cats.slice(0, cats.length - 1);
 }
-
-// Export the cats array and functions
-module.exports = { cats, destructivelyAppendCat, destructivelyPrependCat, destructivelyRemoveLastCat, destructivelyRemoveFirstCat, appendCat, prependCat, removeLastCat, removeFirstCat };
